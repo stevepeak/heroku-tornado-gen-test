6c2d23a217ed6602630d87e16617d149bd62bb59
Steve Peak <steve@stevepeak.net>
2013-02-05 09:04:46 -0700

    added librato and doc

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ import tornado.httpserver
 from genasynchandler import GenAsyncHandler
 from longpooling import LongPoolingHandler
 import os
+import librato
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
@@ -15,7 +16,21 @@ application = tornado.web.Application([
 	(r"/", MainHandler)
 ])
 
+application.librato = librato.LibratoConnection('app11478021@heroku.com', 'be8a47364abd1c426a83628fb8b37cef6efc78abf00afd98692176812ed3e171')
+
 if __name__ == '__main__':
-	http_server = tornado.httpserver.HTTPServer(application)
-	http_server.listen(int(os.environ.get('PORT',5000)))
-	tornado.ioloop.IOLoop.instance().start()
+	print """
+python-sample
+
+http://localhost:5000/
+	gen-async
+		?uri=<http://example.com>
+	long-pool
+		?sleep=<10>
+	"""
+	try:
+		http_server = tornado.httpserver.HTTPServer(application)
+		http_server.listen(int(os.environ.get('PORT',5000)))
+		tornado.ioloop.IOLoop.instance().start()
+	except KeyboardInterrupt:
+		tornado.ioloop.IOLoop.instance().stop()
